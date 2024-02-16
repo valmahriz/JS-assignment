f800795b642223a515ed0ab18c3126b69432de1d
valmahriz <123303571+valmahriz@users.noreply.github.com>
2024-02-16 18:22:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let javaScript = "Hello World";
+console.log(javaScript);
+
+
+// Conditions
+
+let age = 17;
+
+if (age >=18){
+    console.log("Your are Eligible");
+}
+else{
+    console.log("Your are not Eligible");
+}
